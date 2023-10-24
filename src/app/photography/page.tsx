4bb91245f4192fee82fd89599298071b91617d05
Aden Tranter <me@adentranter.com>
2023-10-24 11:48:26 +1000

    Got basic layout sorted; Started adding the links

---
+++ src/app/photography/page.tsx
@@ -0,0 +1,12 @@
+import TopTopNav from "../components/TopTopNav";
+
+export default function Home() {
+  return (
+   <div className="h-screen">
+    <TopTopNav />
+    <div className="container mx-auto mt-5">
+    <h1>Photography</h1>
+      </div>
+    </div>
+  )
+}
